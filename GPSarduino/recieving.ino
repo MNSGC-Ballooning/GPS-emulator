a431a20d3a551c74f205d85427bc100bf5a61d41
Simon P <simonpeterson76@gmail.com>
2017-06-13 16:02:59 -0500

    file reorginization

---
+++ GPSarduino/recieving.ino
@@ -0,0 +1,32 @@
+
+
+
+void readCommand(){
+  static uint8_t ndx = 0;
+  char END = '*';
+  char red;
+  static boolean checksum = false;
+  static int checksumCount = 3;
+  while(GPSserial.available()>0)
+  {
+    red = GPSserial.read();
+    if(red!= END&&!checksum){
+      command[ndx] = red;
+      ndx++;
+    }
+    else if(checksumCount>0){
+      checksum = true;
+      command[ndx] = red;
+      ndx++;
+      checksumCount--;
+    }
+    else{
+      command[ndx] = '\0';
+      ndx = 0;
+      checksumCount = 3;
+      checksum = false;
+      recvd= true;
+    }
+  }
+}
+
