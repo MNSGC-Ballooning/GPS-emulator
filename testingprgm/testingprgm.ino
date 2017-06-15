d0f3ba541ae6f1ab1bb8ea93675e3bb238b27df3
Simon P <simonpeterson76@gmail.com>
2017-06-15 11:26:49 -0500

    prints out lat, long, alt using fakeGPS

--- testingprgm/testingprgm.ino
+++ testingprgm/testingprgm.ino
@@ -3,21 +3,25 @@
 
 SoftwareSerial GPSserial(7,8);
 Adafruit_GPS GPS(&GPSserial);
-
-
+#define MAXMESSAGE 120
+char recieved[MAXMESSAGE];
+int ndx = 0;
 
 void setup() {
 GPS.begin(9600);
 Serial.begin(9600);
+GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
 }
 
 void loop() {
-  for(int i = 0; i<10;i++){
-    GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
-    delay(1000);
-  }
-  GPS.sendCommand("simon_test_command*12");
-  delay(1000);
-
+ while (GPSserial.available() > 0) {
+    GPS.read();
+   }
+ if(GPS.newNMEAreceived()){
+  GPS.parse(GPS.lastNMEA());
+  Serial.println("lat " + String(GPS.latitudeDegrees, 4)+ ", long " + String(GPS.longitudeDegrees, 4) + ", alt" + String(GPS.altitude, 3));
+  ndx = 0;
+ }
+ 
 
 }
